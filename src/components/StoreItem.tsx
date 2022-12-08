9e22e2fd198cdf5aada964002833b2e81f36da07
jiangzy <jiangzy1998@gmail.com>
2022-12-08 21:00:52 +0800

    page

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,16 +1,34 @@
-import { Row, Col } from "react-bootstrap";
-import storeItems from "../data/items.json"
+import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formateCurrency";
 
-export function StoreItem(){
+type StoreItemProps = {
+    id: number,
+    name: string,
+    price: number,
+    imgUrl: string
+}
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+    const quantity = 0;
     return (
-        <>
-            <h1>Store</h1>
-            <Row md={2} xs={1} lg={3} className="g-3">
-                {storeItems.map(item =>(
-                    <Col key={ item.id }>
-                    </Col>
-                ))}
-            </Row>
-        </>        
+        <Card className="h-100">
+            <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }}></Card.Img>
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                    <span className="fs-2">{name}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+                </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <button className="w-100">+ Add To Cart</button>
+                    ) :
+                        <div className="d-flex align-item-center flex-column" style={{ gap:".5rem" }}>
+                            <div className="d-flex align-items-center justfy-content-center" style={{  }}>
+                            </div>
+                        </div>
+                    }
+                </div>
+            </Card.Body>
+        </Card>
     )
 }
