bf7e824bfc9dde7236051af33bd44fa92e747479
jiangzy <jiangzy1998@gmail.com>
2022-12-19 20:08:17 +0800

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./hooks/ShoppingCartContext";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 
 function App() {
   return (
-    <>
-    <Navbar></Navbar>
+    <ShoppingCartProvider>
+      <Navbar></Navbar>
       <Container className="mb-4">
         <Routes>
-          <Route path="/" element={ <Home></Home> }></Route>
-          <Route path="/store" element={ <Store></Store> }></Route>
-          <Route path="/about" element={ <About></About> }></Route>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/store" element={<Store></Store>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
         </Routes>
       </Container>
-    </>
-    
+    </ShoppingCartProvider>
+
   )
 }
 
